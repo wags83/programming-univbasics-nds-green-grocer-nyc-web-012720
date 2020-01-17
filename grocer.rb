25a82af624aa878162eac2360a873c0f4c690c55
Greg Wagner <gpwagner@gmail.com>
2020-01-17 00:27:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 def find_item_by_name_in_collection(name, collection)
 i=0
 found_item = {}
@@ -26,6 +24,7 @@ def consolidate_cart(cart)
       end
       i += 1
     end
+
     cons_cart
   end
 
@@ -75,7 +74,6 @@ while i < cart.length
 end
 
 def checkout(cart, coupons)
-
 cons_cart = []
 coupon_cart = []
 clearance_cart = []
@@ -83,6 +81,7 @@ clearance_cart = []
 cons_cart = consolidate_cart(cart)
 coupon_cart = apply_coupons(cons_cart, coupons)
 clearance_cart = apply_clearance(coupon_cart)
+
 i = 0
 total = 0
 
@@ -91,7 +90,7 @@ while i < clearance_cart.length do
   i += 1
 end
 if total > 100
-  total = 0.9 * total
+  total = 0.8 * total
 end
 total
 end
